c9c74cbf680d486b970a3a74eaa0918e8e42ff20
Ben Tschida <b.tschida12@gmail.com>
2021-03-30 20:55:15 -0700

    Update Weather

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'f69b947f94d2f5e4440bf3178cf5a9c5';
 
 // Set Position function
 setPosition();
